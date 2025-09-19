b19ed8eb45edbfb6fd06c4a8175f28ff158ccfa6
vzorvalpodrugu <Starkovqw8904@gmail.com>
2025-09-19 18:34:24 +0300

    Описан маршрут для "добавить ответ к вопросу"

--- Test/urls.py
+++ Test/urls.py
@@ -3,10 +3,12 @@ from django.urls import path
 from core.views import (
     QuestionListView,
     QuestionDetailView,
+    AnswerCreateView
 )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/questions', QuestionListView.as_view(), name='questions'),
     path('api/questions/<int:id>', QuestionDetailView.as_view(), name='question'),
+    path('api/questions/<int:id>/answers', AnswerCreateView.as_view(), name='answers'),
 ]
