cc0548211bd42f23c3f35a85f8e2bb264a2e119a
vzorvalpodrugu <Starkovqw8904@gmail.com>
2025-09-19 17:23:02 +0300

    Обновлен urls.py

--- Test/urls.py
+++ Test/urls.py
@@ -1,7 +1,8 @@
-
 from django.contrib import admin
 from django.urls import path
+from core.views import QuestionViewSet
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/questions', QuestionViewSet.as_view(), name='questions_create'),
 ]
