b7e83cd6b72751d590bf8d6aaabaff575454480c
vzorvalpodrugu <Starkovqw8904@gmail.com>
2025-09-19 18:02:04 +0300

    Описан AnswerSerializer

--- core/serializers.py
+++ core/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from core.models import Question
+from core.models import Question, Answer
+
 
 class QuestionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Question
         fields = ['id', 'text']
+
+class AnswerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Answer
+        fields = ["id", "question_id", "user_id", "text"]
