910291ceb5c10110c287f84f55a51da60a01f2a2
vzorvalpodrugu <Starkovqw8904@gmail.com>
2025-09-19 17:53:01 +0300

    Описаны следующие методы:
    GET /questions/ — список всех вопросов
    GET /questions/{id} — получить вопрос и все ответы на него
    DELETE /questions/{id} — удалить вопрос (вместе с ответами)

--- Test/urls.py
+++ Test/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from core.views import QuestionViewSet
+from core.views import (
+    QuestionListView,
+    QuestionDetailView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/questions', QuestionViewSet.as_view(), name='questions_create'),
+    path('api/questions', QuestionListView.as_view(), name='questions'),
+    path('api/questions/<int:id>', QuestionDetailView.as_view(), name='question'),
 ]
--- core/views.py
+++ core/views.py
@@ -6,10 +6,12 @@ from .models import Question
 from core.serializers import QuestionSerializer
 from .utils import api_key_required
 
-class QuestionViewSet(APIView):
-
+class QuestionListView(APIView):
     @api_key_required
     def post(self, request):
+        """
+        POST /questions/ — создать новый вопрос
+        """
         serializer = QuestionSerializer(data=request.data)
 
         if serializer.is_valid():
@@ -17,3 +19,49 @@ class QuestionViewSet(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @api_key_required
+    def get(self, request):
+        """
+        GET /questions/ — список всех вопросов
+        """
+        try:
+            questions = Question.objects.all()
+            serializer = QuestionSerializer(questions, many=True)
+            return Response(serializer.data)
+        except Question.DoesNotExist:
+            return Response(
+                {"error": "Вопросов не найдено!"},
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+class QuestionDetailView(APIView):
+    @api_key_required
+    def get(self, request, id):
+        """
+        GET /questions/{id} — получить вопрос и все ответы на него
+        """
+        try:
+            question = Question.objects.get(id=id)
+            serializer = QuestionSerializer(question)
+            return Response(serializer.data)
+        except Question.DoesNotExist:
+            return Response(
+                {"error": f"Вопроса с id={id} не найдено!"},
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+    @api_key_required
+    def delete(self, request, id):
+        """
+         DELETE /questions/{id} — удалить вопрос (вместе с ответами)
+        """
+        try:
+            question = Question.objects.get(id=id)
+            question.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Question.DoesNotExist:
+            return Response(
+                {"error": "Вопроса с id={id} не найдено!"},
+                status=status.HTTP_404_NOT_FOUND
+            )
